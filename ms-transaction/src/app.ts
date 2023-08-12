231c3180a18dccf65468d45b80e3519e13b80d5d
Jose Namoc <namoc.lopez.j@gmail.com>
2023-08-11 22:16:39 -0500

    passed routes to a new file and create first test to check jest installation

--- ms-transaction/src/app.ts
+++ ms-transaction/src/app.ts
@@ -1,16 +1,18 @@
 import express from 'express'
 import envConfig from './config';
 import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import routes from './routes';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/greetings', (req, res) => {
-  res.send('Hola, bienvenido a mi servidor Express!');
-});
+app.use(helmet());
+app.use(routes)
 
 app.listen(envConfig.PORT, () => {
   console.log(`El servidor está escuchando en el puerto ${envConfig.PORT}`);
 });
+
+export default app
